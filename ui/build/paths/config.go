4455ed6235716e643b56ed2867d2bb341a010977
Saikat Halder <iamdevsaikat@gmail.com>
2020-04-08 23:11:44 +0530

    soong: Add 'python3.8' to allowed commands
    Signed-off-by: Saikat Halder <iamdevsaikat@gmail.com>

--- ui/build/paths/config.go
+++ ui/build/paths/config.go
@@ -100,6 +100,7 @@ var Configuration = map[string]PathConfig{
 	"pstree":   Allowed,
 	"python3":  Allowed,
 	"python3.6":  Allowed,
+	"python3.8":  Allowed,
 	"realpath": Allowed,
 	"repo":     Allowed,
 	"rsync":    Allowed,
